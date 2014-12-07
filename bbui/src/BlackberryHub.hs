60640426c959ab238bc7e66f496419a5ecd05234
Stephen Paul Weber <singpolyma@singpolyma.net>
2014-12-07 17:09:14 -0500

    Refactor hub code

--- bbui/src/BlackberryHub.hs
+++ bbui/src/BlackberryHub.hs
@@ -25,41 +25,39 @@ data HubRequest = AddHubAccount Text | RemoveHubAccount Text deriving (Show, Eq)
 
 newtype AccountID = AccountID CLLong deriving (Show, Eq, Ord)
 
+foreign import ccall safe "hub.cpp hub_init"
+	c_hub_init ::
+	CString -> CString ->
+	IO CInt
+
 foreign import ccall safe "hub.cpp hub_setup_account"
 	c_hub_setup_account ::
-	CString -> CString -> CString -> CString -> CString ->
+	CString -> CString -> CString ->
 	IO AccountID
 
 foreign import ccall safe "hub.cpp hub_remove_account"
 	c_hub_remove_account ::
-	AccountID -> CString -> CString ->
+	AccountID ->
 	IO CInt
 
-owner :: BS.ByteString
-owner = T.encodeUtf8 $ T.pack "net.singpolyma.txtmpp"
-
-assets :: BS.ByteString
-assets = T.encodeUtf8 $ T.pack "/apps/net.singpolyma.txtmpp.testDev_lyma_txtmpp4fc765cb/public/assets-public/"
-
 hubServer :: TChan HubRequest -> IO ()
-hubServer chan =
-	evalStateT (forever $ lift (atomically $ readTChan chan) >>= msg) empty
+hubServer chan = evalContT $ do
+	owner <- ContT $ BS.useAsCString $ T.encodeUtf8 $ T.pack "net.singpolyma.txtmpp"
+	assets <- ContT $ BS.useAsCString $ T.encodeUtf8 $ T.pack "/apps/net.singpolyma.txtmpp.testDev_lyma_txtmpp4fc765cb/public/assets-public/"
+	void $ lift $ c_hub_init owner assets -- TODO: log error?
+
+	lift $ evalStateT (forever $ lift (atomically $ readTChan chan) >>= msg) empty
 	where
 	msg (AddHubAccount jid) = mapStateT evalContT $ do
 		csjid <- lift $ ContT $ BS.useAsCString (T.encodeUtf8 jid)
 		displayName <- lift $ ContT $ BS.useAsCString (T.encodeUtf8 $ T.pack "txtmpp")
 		icon <- lift $ ContT $ BS.useAsCString (T.encodeUtf8 $ T.pack "hub.png")
-		owner <- lift $ ContT $ BS.useAsCString owner
-		assets <- lift $ ContT $ BS.useAsCString assets
 
-		accountId <- lift $ lift $ c_hub_setup_account csjid displayName icon owner assets
+		accountId <- lift $ lift $ c_hub_setup_account csjid displayName icon
 		if accountId < AccountID 0 then return () else -- TODO: Log error?
 			modify (Map.insert jid accountId)
 	msg (RemoveHubAccount jid) = mapStateT evalContT $ do
-		owner <- lift $ ContT $ BS.useAsCString owner
-		assets <- lift $ ContT $ BS.useAsCString assets
-
 		accountId <- Map.lookup jid <$> get
 		case accountId of
-			Just id -> void $ lift $ lift $ c_hub_remove_account id owner assets -- TODO: Log error?
+			Just id -> void $ lift $ lift $ c_hub_remove_account id -- TODO: Log error?
 			Nothing -> return () -- TODO: Log error?
--- bbui/src/hub.cpp
+++ bbui/src/hub.cpp
@@ -1,5 +1,4 @@
 #include <QList>
-#include <QtDebug>
 
 #include <bb/pim/account/Result>
 #include <bb/pim/account/Account>
@@ -8,40 +7,77 @@
 
 #include "bb/pim/unified/unified_data_source.h"
 
-/* This code is not threadsafe -- only invoke the hub from one thread */
-
 extern "C" {
 
+/* This code is not threadsafe -- only invoke the hub from one thread */
 static uds_context_t handle = NULL;
+static const char *owner = NULL;
+static const char *assets = NULL;
 
-static void reinit(const char *owner, const char *assets) {
+static int reinit() {
 	if(handle) uds_close(&handle);
 	handle = NULL;
 
 	if(uds_init(&handle, false) != UDS_SUCCESS) {
 		handle = NULL;
-		qDebug() << "uds_init FAILED";
+		return -1;
 	} else {
 		int r = UDS_ERROR_TIMEOUT;
 		while(r == UDS_ERROR_TIMEOUT) {
 			r = uds_register_client(handle, owner, "", assets);
-			if(r == UDS_ERROR_DISCONNECTED) reinit(owner, assets);
+			if(r == UDS_ERROR_DISCONNECTED) reinit();
 		}
 
-		if(r == UDS_SUCCESS) return;
-		qDebug() << "uds_register_client FAILED with: " << r;
+		if(r == UDS_SUCCESS) return 0;
+		return -2;
 	}
 }
 
-Q_DECL_EXPORT int hub_remove_account(long long int accountId, const char *owner, const char *assets) {
-	if(!handle) reinit(owner, assets);
+/* XXX: We don't dup the strings for now, so be sure to keep them around! */
+Q_DECL_EXPORT int hub_init(const char *_owner, const char *_assets) {
+	owner = _owner;
+	assets = _assets;
+	return reinit();
+}
+
+Q_DECL_EXPORT int hub_update_account(uds_account_data_t *account) {
+	if(!handle) reinit();
+	if(!handle) return -1;
+
+	int r = UDS_ERROR_TIMEOUT;
+
+	while(r == UDS_ERROR_TIMEOUT) {
+		r = uds_account_updated(handle, account);
+		if(r == UDS_ERROR_DISCONNECTED) {
+			reinit();
+			if(!handle) return -1;
+		}
+	}
+
+	if(r == UDS_SUCCESS) return 0;
+
+	r = UDS_ERROR_TIMEOUT;
+	while(r == UDS_ERROR_TIMEOUT) {
+		r = uds_account_added(handle, account);
+		if(r == UDS_ERROR_DISCONNECTED) {
+			reinit();
+			if(!handle) return -1;
+		}
+	}
+
+	if(r == UDS_SUCCESS) return 0;
+	return -2;
+}
+
+Q_DECL_EXPORT int hub_remove_account(long long int accountId) {
+	if(!handle) reinit();
 	if(!handle) return -1;
 
 	int r = UDS_ERROR_TIMEOUT;
 	while(r == UDS_ERROR_TIMEOUT) {
 		r = uds_account_removed(handle, accountId);
 		if(r == UDS_ERROR_DISCONNECTED) {
-			reinit(owner, assets);
+			reinit();
 			if(!handle) return -1;
 		}
 	}
@@ -53,7 +89,36 @@ Q_DECL_EXPORT int hub_remove_account(long long int accountId, const char *owner,
 	return 0;
 }
 
-static long long int getAccountId(const QString &username, const QString &displayName, const char *owner, const char *assets) {
+Q_DECL_EXPORT int hub_update_item(uds_inbox_item_data_t *item) {
+	if(!handle) reinit();
+	if(!handle) return -1;
+
+	int r = UDS_ERROR_TIMEOUT;
+
+	while(r == UDS_ERROR_TIMEOUT) {
+		r = uds_item_updated(handle, item);
+		if(r == UDS_ERROR_DISCONNECTED) {
+			reinit();
+			if(!handle) return -1;
+		}
+	}
+
+	if(r == UDS_SUCCESS) return 0;
+
+	r = UDS_ERROR_TIMEOUT;
+	while(r == UDS_ERROR_TIMEOUT) {
+		r = uds_item_added(handle, item);
+		if(r == UDS_ERROR_DISCONNECTED) {
+			reinit();
+			if(!handle) return -1;
+		}
+	}
+
+	if(r == UDS_SUCCESS) return 0;
+	return -2;
+}
+
+Q_DECL_EXPORT long long int hub_find_account_id(const char *username) {
 	bb::pim::account::Account account;
 	bb::pim::account::AccountService service;
 
@@ -65,7 +130,7 @@ static long long int getAccountId(const QString &username, const QString &displa
 		if(it->isExternalData() && it->externalSetupInvokeTarget() == owner && it->settingsProperty("user") == username) {
 			if(found) {
 				int r = 0;
-				if((r = hub_remove_account(it->id(), owner, assets)) < 0) return r;
+				if((r = hub_remove_account(it->id())) < 0) return r;
 			} else {
 				found = true;
 				account = *it;
@@ -75,34 +140,25 @@ static long long int getAccountId(const QString &username, const QString &displa
 
 	if(found) {
 		account.setExternalData(true);
-		account.setDisplayName(displayName);
 		account.setSettingsValue("user", username);
 		account.setExternalSetupInvokeTarget(owner);
 		bb::pim::account::Result r = service.updateAccount(account.id(), account);
-		if(!r.isSuccess()) {
-			qDebug() << "updateAccount FAILED: " << r.message();
-			return -4;
-		}
+		if(!r.isSuccess()) return -4;
 	} else {
 		bb::pim::account::Provider external = service.provider("external");
 		account = bb::pim::account::Account(external);
 		account.setExternalData(true);
-		account.setDisplayName(displayName);
 		account.setSettingsValue("user", username);
 		account.setExternalSetupInvokeTarget(owner);
 		bb::pim::account::Result r = service.createAccount(external.id(), account);
-		if(!r.isSuccess()) {
-			qDebug() << "createAccount FAILED: " << r.message();
-			return -4;
-		}
+		if(!r.isSuccess()) return -4;
 	}
 
 	return account.id();
 }
 
-Q_DECL_EXPORT int hub_setup_account(const char *username, const char *displayName, const char *icon, const char *owner, const char *assets) {
-	int r = 0;
-	long long int accountId = getAccountId(username, displayName, owner, assets);
+Q_DECL_EXPORT long long int hub_setup_account(const char *username, const char *displayName, const char *icon) {
+	long long int accountId = hub_find_account_id(username);
 
 	if(accountId < 0) return accountId;
 
@@ -114,44 +170,11 @@ Q_DECL_EXPORT int hub_setup_account(const char *username, const char *displayNam
 	uds_account_data_set_icon(account_data, icon);
 	uds_account_data_set_target_name(account_data, owner);
 
-	if(!handle) reinit(owner, assets);
-	if(!handle) {
-		r = -1;
-		goto cleanup;
-	}
+	int r = hub_update_account(account_data);
 
-	r = UDS_ERROR_TIMEOUT;
-	while(r == UDS_ERROR_TIMEOUT) {
-		r = uds_account_updated(handle, account_data);
-		if(r == UDS_ERROR_DISCONNECTED) {
-			reinit(owner, assets);
-			if(!handle) {
-				r = -1;
-				goto cleanup;
-			}
-		}
-	}
-
-	if(r == UDS_SUCCESS) goto cleanup;
-
-	r = UDS_ERROR_TIMEOUT;
-	while(r == UDS_ERROR_TIMEOUT) {
-		r = uds_account_added(handle, account_data);
-		if(r == UDS_ERROR_DISCONNECTED) {
-			reinit(owner, assets);
-			if(!handle) {
-				r = -1;
-				goto cleanup;
-			}
-		}
-	}
-
-	if(r != UDS_SUCCESS) r = -2;
-
-cleanup:
 	uds_account_data_destroy(account_data);
 
-	if(r == UDS_SUCCESS) return accountId;
+	if(r == 0) return accountId;
 	return r;
 }
 
